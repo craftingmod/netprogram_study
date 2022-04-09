ffd3f83f6daa58634919ed5253dad244a0a91e56
craftingmod <alyac1234@gmail.com>
2022-04-09 22:59:51 +0900

    [Chore] Iphdr-Test connection to localhost

--- socket4/pack1.py
+++ socket4/pack1.py
@@ -1,5 +1,6 @@
 import binascii
 from iphdr import Iphdr
+import socket
 
 ip = Iphdr(1000, 6, "10.0.0.1", "11.0.0.1")
 packed_iphdr = ip.pack_Iphdr()
@@ -8,3 +9,6 @@ print(binascii.b2a_hex(packed_iphdr))
 unpacked_iphdr = Iphdr.unpack_Iphdr(packed_iphdr)
 print(unpacked_iphdr)
 print(f"Packet Size: {Iphdr.getPacketSize(unpacked_iphdr)}, Protocol ID: {Iphdr.getProtocolId(unpacked_iphdr)}, IP: {Iphdr.getIP(unpacked_iphdr)}")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(packed_iphdr, ("localhost", 2500))
