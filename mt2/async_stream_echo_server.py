f872b311f3e651e503a68539a603d52d209d02db
craftingmod <alyac1234@gmail.com>
2022-05-17 00:20:03 +0900

    [Lesson] Week 9 - async operation

---
+++ mt2/async_stream_echo_server.py
@@ -0,0 +1,27 @@
+import asyncio
+
+port = 2500
+BUFSIZE = 1024
+
+async def handle_echo(reader, writer):
+  while True:
+    data = await reader.read(BUFSIZE)
+    addr = writer.get_extra_info("peername")
+    print(f"Received {data.decode()!r} from {addr!r}")
+    if not data:
+      break
+    
+    writer.write(data)
+
+    await writer.drain()
+    print(f"Send: {data.decoe()!r}")
+
+async def main():
+  server = await asyncio.start_server(handle_echo, "", port)
+
+  addr = server.sockets[0].getsockname()
+  print(f"Serving on {addr}")
+
+  await server.serve_forever()
+
+asyncio.run(main())
